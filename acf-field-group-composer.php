9fd36f0cb44a1cae54283d97d158ea1b99607c86
Zachary Gibb <zachgibb@users.noreply.github.com>
2025-06-10 18:11:13 -0400

    [fix] actually properly tag release

--- acf-field-group-composer.php
+++ acf-field-group-composer.php
@@ -7,7 +7,7 @@
  * Author URI:      https://bleech.de
  * Text Domain:     acf-field-group-composer
  * Domain Path:     /languages
- * Version:         1.2.0
+ * Version:         1.3.0
  *
  * @package         ACF_Field_Group_Composer
  */
